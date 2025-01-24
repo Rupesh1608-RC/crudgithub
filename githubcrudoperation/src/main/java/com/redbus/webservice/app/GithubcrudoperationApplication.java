44aff2aff9873d71f80a6129446023e998bdd8a2
Shree <Shree@DESKTOP-9SF6INJ>
2025-01-24 11:44:14 +0530

    add method m1 and m2

---
+++ githubcrudoperation/src/main/java/com/redbus/webservice/app/GithubcrudoperationApplication.java
@@ -0,0 +1,13 @@
+package com.redbus.webservice.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubcrudoperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubcrudoperationApplication.class, args);
+	}
+
+}
---
+++ githubcrudoperation/src/main/java/com/redbus/webservice/app/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.redbus.webservice.app.controller;
+
+public class HomeController {
+	
+	
+	public void m1() {
+		
+	}
+	
+	public void m2() {
+		
+	}
+
+}
